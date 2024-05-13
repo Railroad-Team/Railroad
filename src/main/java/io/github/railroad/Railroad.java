50f19ba5904bd7cd9c43562ab25ce56da9943e63
YodaForce157 <Max.bennett@21stcenturyit.com>
2024-05-13 18:50:38 +0100

    Update Railroad.java

--- src/main/java/io/github/railroad/Railroad.java
+++ src/main/java/io/github/railroad/Railroad.java
@@ -102,7 +102,7 @@ public class Railroad extends Application {
         // TODO: Find a better way to calculate these because it makes it weird on different sized monitors
         double windowW = Math.max(500, Math.min(screenW * 0.75, 1024));
         double windowH = Math.max(500, Math.min(screenH * 0.75, 768));
-        
+
         //TODO Possible start to a better way?
         //double windowW = screen.getOutputScaleX() * (screenW * 0.50);
         //double windowH = screen.getOutputScaleY() * (screenH * 0.55);
