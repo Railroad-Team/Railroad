e0c1a33edce0e2d233cc5d508e2e536bc377eca6
Lanpingner <45770525+Lanpingner@users.noreply.github.com>
2024-05-27 17:42:22 +0200

    Update src/main/java/io/github/railroad/plugin/defaults/github/ui/GithubProfilesListCell.java
    Co-authored-by: TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>

--- src/main/java/io/github/railroad/plugin/defaults/github/ui/GithubProfilesListCell.java
+++ src/main/java/io/github/railroad/plugin/defaults/github/ui/GithubProfilesListCell.java
@@ -13,7 +13,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import io.github.railroad.ui.defaults.RRStackPane;
 
 public class GithubProfilesListCell extends ListCell<Profile> {
     private final RRStackPane node = new RRStackPane();
