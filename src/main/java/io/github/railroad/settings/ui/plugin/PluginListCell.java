638f4d4460b50ab0528dd9b8d5d19ba10fc5d93d
Lanpingner <45770525+Lanpingner@users.noreply.github.com>
2024-05-27 17:48:18 +0200

    Update src/main/java/io/github/railroad/settings/ui/plugin/PluginListCell.java
    Co-authored-by: TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>

--- src/main/java/io/github/railroad/settings/ui/plugin/PluginListCell.java
+++ src/main/java/io/github/railroad/settings/ui/plugin/PluginListCell.java
@@ -10,8 +10,6 @@ import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 
 public class PluginListCell extends ListCell<Plugin> {
     private final StackPane node = new StackPane();
