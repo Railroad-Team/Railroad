ade91d0edb67dedb58ea920c26fb4efae84c88a6
YodaForce157 <Max.bennett@21stcenturyit.com>
2024-05-29 17:45:13 +0100

    Remove ProjectSearchField

--- src/main/java/io/github/railroad/project/ui/create/widget/ProjectSearchField.java
+++
@@ -1,9 +0,0 @@
-package io.github.railroad.project.ui.create.widget;
-
-import io.github.railroad.ui.localized.LocalizedTextField;
-
-public class ProjectSearchField extends LocalizedTextField {
-    public ProjectSearchField() {
-        super("railroad.home.welcome.projectsearch");
-    }
-}
