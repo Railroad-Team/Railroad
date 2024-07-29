cf8d1fa75f87ef045a47edd6caa5434152fd2271
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2024-07-29 14:30:58 +0100

    Some code editor changes
    Line Number Styling
    Adjustable Font Size
    Error Tooltips
    Now shows start of file instead of end when you open it

--- src/main/java/io/github/railroad/ide/CodeEditorPane.java
+++ src/main/java/io/github/railroad/ide/CodeEditorPane.java
@@ -2,14 +2,29 @@ package io.github.railroad.ide;
 
 import io.github.railroad.Railroad;
 import io.github.railroad.ide.syntax_tests.TreeSitterJavaEditorPane;
+import io.github.railroad.ui.defaults.RRHBox;
 import io.github.railroad.utility.ShutdownHooks;
 import io.github.railroad.utility.compiler.JavaSourceFromString;
+import javafx.collections.FXCollections;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableMap;
 import javafx.concurrent.Task;
+import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Popup;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
-import org.fxmisc.richtext.model.Paragraph;
+import org.fxmisc.richtext.event.MouseOverTextEvent;
 import org.fxmisc.richtext.model.StyleSpans;
-import org.fxmisc.richtext.model.StyleSpansBuilder;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 import javax.tools.*;
 import java.io.IOException;
@@ -20,20 +35,91 @@ import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.IntFunction;
 
 public class CodeEditorPane extends CodeArea {
+    private static final JavaCompiler JAVA_COMPILER = ToolProvider.getSystemJavaCompiler();
+
     private final Path filePath;
     private final ExecutorService executor0 = Executors.newFixedThreadPool(2);
-    private static final JavaCompiler JAVA_COMPILER = ToolProvider.getSystemJavaCompiler();
+    private final ObservableMap<Diagnostic<? extends JavaFileObject>, Popup> errors = FXCollections.observableHashMap();
+
+    private static final int[] FONT_SIZES = {6, 8, 10, 12, 14, 16, 18, 20, 24, 26, 28, 30, 36, 40, 48, 56, 60};
+    private int fontSizeIndex = 5;
 
     public CodeEditorPane(Path item) {
         this.filePath = item;
 
-        setParagraphGraphicFactory(LineNumberFactory.get(this));
+        setParagraphGraphicFactory(LineNumberFactory.get(this)); // marginErrors();
+        setMouseOverTextDelay(Duration.ofMillis(500));
+
         syntaxHighlight();
         errorHighlighting();
         codeCompletion();
         listenForChanges();
+        resizableFont();
+
+        moveTo(0);
+    }
+
+    private void marginErrors() {
+        IntFunction<? extends Node> lineNumberFactory = LineNumberFactory.get(this);
+        setParagraphGraphicFactory(value -> {
+            var hbox = new RRHBox();
+            hbox.setMinWidth(50);
+            Node node = lineNumberFactory.apply(value);
+            if(node instanceof Label label) {
+                label.setTextAlignment(TextAlignment.LEFT);
+            }
+
+            hbox.getChildren().add(node);
+
+            int line = value + 1;
+            List<Diagnostic<? extends JavaFileObject>> diagnostics = errors.keySet().stream()
+                    .filter(diagnostic -> diagnostic.getLineNumber() == line)
+                    .toList();
+
+            if (diagnostics.isEmpty()) {
+                hbox.setPadding(new Insets(0, 5, 0, 0));
+                hbox.setMinWidth(Region.USE_PREF_SIZE);
+                return hbox;
+            }
+
+            var icon = new FontIcon(FontAwesomeSolid.EXCLAMATION_TRIANGLE);
+            icon.setIconColor(Color.RED);
+            var popup = new Popup();
+            popup.getContent().add(diagnostics.size() == 1 ? new DiagnosticPane(diagnostics.getFirst()) : new DiagnosticPane(diagnostics));
+
+            icon.setOnMouseEntered(event -> {
+                Point2D screenPosition = icon.localToScreen(0, 0);
+                popup.show(icon, screenPosition.getX(), screenPosition.getY());
+            });
+
+            icon.setOnMouseExited(event -> popup.hide());
+
+            hbox.getChildren().addAll(icon);
+            return hbox;
+        });
+    }
+
+    private void resizableFont() {
+        setStyle("-fx-font-size: " + FONT_SIZES[fontSizeIndex] + "px;");
+
+        setOnKeyPressed(event -> {
+            if (!event.isControlDown())
+                return;
+
+            KeyCode code = event.getCode();
+            if (code != KeyCode.EQUALS && code != KeyCode.MINUS)
+                return;
+
+            int index = fontSizeIndex + (code == KeyCode.EQUALS ? 1 : -1);
+            if (index < 0 || index >= FONT_SIZES.length)
+                return;
+
+            fontSizeIndex = index;
+            setStyle("-fx-font-size: " + FONT_SIZES[fontSizeIndex] + "px;");
+        });
     }
 
     private void codeCompletion() {
@@ -66,6 +152,12 @@ public class CodeEditorPane extends CodeArea {
                     }
                 })
                 .subscribe(this::applyErrorHighlighting);
+
+        this.errors.addListener((MapChangeListener<Diagnostic<? extends JavaFileObject>, Popup>) change -> {
+            if (change.wasRemoved()) {
+                change.getValueRemoved().hide();
+            }
+        });
     }
 
     private Task<DiagnosticCollector<JavaFileObject>> requestErrorDiagnostics() {
@@ -86,18 +178,46 @@ public class CodeEditorPane extends CodeArea {
 
     private void applyErrorHighlighting(DiagnosticCollector<JavaFileObject> diagnostics) {
         long startTime = System.currentTimeMillis();
-        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
-        record StyleRange(int start, int end, String style) {}
 
-        List<StyleRange> styleRanges = new ArrayList<>(diagnostics.getDiagnostics().stream()
+        Map<Diagnostic<? extends JavaFileObject>, Popup> errors = diagnostics.getDiagnostics().stream()
                 .filter(diagnostic -> diagnostic.getKind() == Diagnostic.Kind.ERROR)
-                .map(diagnostic -> new StyleRange((int) diagnostic.getStartPosition(), (int) diagnostic.getEndPosition() + 1, "error"))
-                .sorted(Comparator.comparingInt(range -> range.start))
-                .toList());
+                .sorted(Comparator.comparingLong(Diagnostic::getStartPosition))
+                .collect(HashMap::new, (map, diagnostic) -> {
+                    int start = (int) diagnostic.getStartPosition();
+                    int end = (int) diagnostic.getEndPosition();
+                    String message = diagnostic.getMessage(null);
 
-        for (StyleRange styleRange : styleRanges) {
-            setStyleClass(styleRange.start, styleRange.end, styleRange.style);
-        }
+                    var popup = new Popup();
+                    popup.getContent().add(new DiagnosticPane(diagnostic));
+
+                    // show the popup if the mouse hovers over the error
+                    addEventHandler(MouseOverTextEvent.MOUSE_OVER_TEXT_BEGIN, event -> {
+                        int position = event.getCharacterIndex();
+                        if (position >= start && position <= end) {
+                            Point2D screenPosition = event.getScreenPosition();
+                            popup.show(CodeEditorPane.this, screenPosition.getX(), screenPosition.getY());
+                        }
+                    });
+
+                    // hide the popup if the mouse leaves the error
+                    addEventHandler(MouseEvent.MOUSE_MOVED, event -> {
+                        if (!popup.isShowing())
+                            return;
+
+                        int charIndex = CodeEditorPane.this.hit(event.getX(), event.getY()).getCharacterIndex().orElse(-1);
+                        if (charIndex < start || charIndex > end) {
+                            popup.hide();
+                        }
+                    });
+
+                    map.put(diagnostic, popup);
+                    setStyleClass(start, end, "error");
+
+                    Railroad.LOGGER.error("Error at L{}:{} - {}", diagnostic.getLineNumber(), diagnostic.getColumnNumber(), message);
+                }, HashMap::putAll);
+
+        this.errors.clear();
+        this.errors.putAll(errors);
 
         Railroad.LOGGER.info("Error highlighting took {}ms", System.currentTimeMillis() - startTime);
     }
