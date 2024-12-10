f7985bb0692d42e90dd2e351cb0eec3d7719e5d1
YodaForce157 <Max.bennett@21stcenturyit.com>
2024-12-10 10:02:07 +0000

    Settings class and basic event handler (FIXME)

--- src/main/java/io/github/railroad/settings/handler/Setting.java
+++ src/main/java/io/github/railroad/settings/handler/Setting.java
@@ -1,5 +1,8 @@
 package io.github.railroad.settings.handler;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import lombok.Getter;
 
 @Getter
@@ -8,13 +11,26 @@ public class Setting<T> {
     private final String id;
     private final T defaultValue;
 
-    public Setting(String id, Class<T> type, T defaultValue) {
+    private final EventType eventType;
+    private final EventHandler eventHandler;
+
+    private SimpleObjectProperty<T> value = new SimpleObjectProperty<>();
+    //TODO allow multiple event handlers
+    public Setting(String id, Class<T> type, T defaultValue, EventType eventType, EventHandler<?> eventHandler) {
         this.id = id;
         this.type = type;
         this.defaultValue = defaultValue;
+        this.eventType = eventType;
+        this.eventHandler = eventHandler;
+
+        setValue(defaultValue);
+    }
+
+    public T getValue() {
+        return value.getValue();
     }
 
     public void setValue(T value) {
-
+        this.value.setValue(value);
     }
 }
--- src/main/java/io/github/railroad/settings/handler/SettingsManager.java
+++ src/main/java/io/github/railroad/settings/handler/SettingsManager.java
@@ -5,22 +5,20 @@ import io.github.railroad.Railroad;
 import io.github.railroad.localization.Language;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 
 public class SettingsManager {
-    private final ObservableMap<String, Setting<?>> settings = FXCollections.observableHashMap();
     private final ObservableMap<Class<?>, SettingCodec<?, ?, ?>> codecs = FXCollections.observableHashMap();
+    private final Settings settings = new Settings();
 
     public SettingsManager() {
         defaultCodecs();
         defaultSettings();
-    }
-
-    public void registerSetting(Setting<?> setting) {
-        Railroad.LOGGER.info("Registering setting: {}", setting.getId());
-        settings.put(setting.getId(), setting);
     }
 
     public void registerCodec(SettingCodec<?, ?, ?> codec) {
@@ -32,11 +30,36 @@ public class SettingsManager {
     }
 
     public Setting<?> getSetting(String id) {
-        return settings.get(id);
+        return settings.getSetting(id);
     }
 
     public SettingCodec<?, ?, ?> getCodec(Class<?> type) {
         return codecs.get(type);
+    }
+
+    public TreeView createTree() {
+        var tree = new TreeView<>(new TreeItem<>("Settings"));
+
+        for (String id : settings.getSettingsMap().keySet()) {
+            var setting = getSetting(id);
+            SettingCodec codec = getCodec(setting.getType());
+
+            if (codec == null) {
+                Railroad.LOGGER.warn("No codec found for setting: {}", id);
+                continue;
+            }
+
+            var item = codec.getNodeCreator().apply(setting.getDefaultValue());
+            ((Node)item).addEventHandler(setting.getEventType(), setting.getEventHandler());
+            ((Node)item).addEventHandler(ActionEvent.ANY, e -> {
+                //FIXME stupid generics
+                setting.setValue(codec.getNodeToValFunction().apply(item));
+            });
+
+            tree.getRoot().getChildren().add(new TreeItem(item));
+        }
+
+        return tree;
     }
 
     private void defaultCodecs() {
@@ -65,25 +88,9 @@ public class SettingsManager {
 
     private void defaultSettings() {
         Railroad.LOGGER.info("Registering default settings");
-        registerSetting(new Setting<>("railroad:language.select", Language.class, Language.EN_US));
-    }
-
-    public TreeView createTree() {
-        var tree = new TreeView<>(new TreeItem<>("Settings"));
-
-        for (String id : settings.keySet()) {
-            var setting = getSetting(id);
-            var codec = getCodec(setting.getType());
-
-            if (codec == null) {
-                Railroad.LOGGER.warn("No codec found for setting: {}", id);
-                continue;
-            }
-
-            var item = codec.getNodeCreator().apply(setting.getDefaultValue());
-            tree.getRoot().getChildren().add(new TreeItem(item));
-        }
-
-        return tree;
+        settings.registerSetting(
+                new Setting<>("railroad:language.select", Language.class, Language.EN_US,
+                        Event.ANY,
+                        event -> Railroad.LOGGER.info("Language changed to: {}", ((ComboBox) event.getSource()).getValue())));
     }
 }
