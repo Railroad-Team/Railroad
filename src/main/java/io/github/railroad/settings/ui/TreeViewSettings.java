fd37d2990df8f27935f1d75d45e3e2ff7a59e80b
YodaForce157 <Max.bennett@21stcenturyit.com>
2024-12-22 11:46:21 +0000

    attempt to make it look better

--- src/main/java/io/github/railroad/settings/ui/TreeViewSettings.java
+++ src/main/java/io/github/railroad/settings/ui/TreeViewSettings.java
@@ -6,12 +6,16 @@ import io.github.railroad.ui.defaults.RRHBox;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class TreeViewSettings {
     private Stage stage;
 
+    //TODO make it, well, not look like this - fix button positioning, treeview border etc
     public TreeViewSettings() {
         stage = new Stage();
 
@@ -19,21 +23,29 @@ public class TreeViewSettings {
 
         var vbox = new VBox();
         vbox.setAlignment(Pos.TOP_CENTER);
+        vbox.setMinHeight(700);
 
         var tree = Railroad.SETTINGS_HANDLER.createTree();
         tree.setPrefHeight(500);
 
+        var subBox = new VBox();
+        subBox.setPadding(new Insets(25));
+
         var buttons = new RRHBox();
         buttons.setAlignment(Pos.BOTTOM_RIGHT);
-        buttons.setPadding(new Insets(5));
+        buttons.setPadding(new Insets(25));
+
+        subBox.getChildren().add(buttons);
+
         var button = new LocalizedButton("railroad.generic.apply");
         button.setAlignment(Pos.BOTTOM_RIGHT);
         button.setStyle("-fx-background-color: -color-accent-6");
         button.setOnAction(event -> Railroad.SETTINGS_HANDLER.saveSettingsFile());
 
         buttons.getChildren().add(button);
+        buttons.setAlignment(Pos.BOTTOM_RIGHT);
 
-        vbox.getChildren().addAll(tree, button);
+        vbox.getChildren().addAll(tree,subBox);
         var scene = new Scene(vbox, 600, 700);
 
         Railroad.handleStyles(scene);
--- src/main/java/io/github/railroad/settings/ui/themes/ThemeDownloadPane.java
+++ src/main/java/io/github/railroad/settings/ui/themes/ThemeDownloadPane.java
@@ -1,6 +1,5 @@
 package io.github.railroad.settings.ui.themes;
 
-import io.github.railroad.Railroad;
 import io.github.railroad.localization.ui.LocalizedLabel;
 import io.github.railroad.ui.defaults.RRListView;
 import io.github.railroad.ui.defaults.RRVBox;
@@ -36,7 +35,6 @@ public class ThemeDownloadPane {
         pane.setAlignment(Pos.TOP_CENTER);
 
         var scene = new Scene(pane, 450, 450);
-        Railroad.handleStyles(scene);
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setResizable(false);
         stage.setScene(scene);
