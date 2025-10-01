2e8257c074fa1796e53b69403e1ddb8bc31d016f
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-09-30 17:07:25 -0700

    fix missing libs

--- flake.nix
+++ flake.nix
@@ -47,6 +47,8 @@
               javafx
 
               libGL
+              alsa-lib
+
               gtk3
               glib
               gsettings-desktop-schemas
@@ -64,7 +66,7 @@
             in
             ''
               export JAVAFX_MODULE_PATH=${pkgs.javafx}/lib
-              export LD_LIBRARY_PATH="${pkgs.libGL}/lib:${pkgs.gtk3}/lib:${pkgs.glib.out}/lib:${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXtst}/lib:${pkgs.xorg.libXi}/lib:${pkgs.xorg.libXxf86vm}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.libGL}/lib:${pkgs.gtk3}/lib:${pkgs.glib.out}/lib:${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXtst}/lib:${pkgs.xorg.libXi}/lib:${pkgs.xorg.libXxf86vm}/lib:${pkgs.alsa-lib}/lib:$LD_LIBRARY_PATH"
               export JAVA_TOOL_OPTIONS="${loadLombok}${prev}"
               export GSETTINGS_SCHEMA_DIR="${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas"
 
