a21998086735f1ca21e1c1cd3539c2bcf705c374
Alex <TeenagedLime3@gmail.com>
2025-11-07 14:40:19 +0000

    Adds fullscreen toggle functionality
    - Implements a fullscreen toggle feature using the F11 key or a menu item.
    - This allows users to easily switch between windowed and fullscreen modes.
    - The key can be changed in the keybinds setting menu.

--- src/main/java/dev/railroadide/railroad/settings/keybinds/Keybinds.java
+++ src/main/java/dev/railroadide/railroad/settings/keybinds/Keybinds.java
@@ -3,11 +3,13 @@ package dev.railroadide.railroad.settings.keybinds;
 import dev.railroadide.core.settings.keybinds.Keybind;
 import dev.railroadide.core.settings.keybinds.KeybindCategory;
 import dev.railroadide.core.settings.keybinds.KeybindContexts;
+import dev.railroadide.railroad.Railroad;
 import dev.railroadide.railroad.ide.projectexplorer.PathItem;
 import dev.railroadide.railroad.ide.projectexplorer.ProjectExplorerPane;
 import javafx.scene.control.TreeView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
 
 public class Keybinds {
     public static final Keybind COPY = KeybindHandler.registerKeybind(Keybind.builder()
@@ -29,6 +31,16 @@ public class Keybinds {
             @SuppressWarnings("unchecked")
             TreeView<PathItem> tree = (TreeView<PathItem>) node;
             ProjectExplorerPane.paste(tree.getSelectionModel().getSelectedItem().getValue());
+        })
+        .build());
+
+    public static final Keybind FULLSCREEN = KeybindHandler.registerKeybind(Keybind.builder()
+        .id("railroad:fullscreen")
+        .category(new KeybindCategory("railroad:general", "railroad.settings.keybinds.category.general"))
+        .addDefaultKey(KeyCode.F11)
+        .addAction(KeybindContexts.of("railroad:ide"), $ -> {
+            Stage primaryStage = Railroad.WINDOW_MANAGER.getPrimaryStage();
+            primaryStage.setFullScreen(!primaryStage.isFullScreen());
         })
         .build());
 
