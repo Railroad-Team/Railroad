5e8446b7a53583bbfdab33e29c6af87881e72ae0
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2025-11-07 14:50:58 +0000

    Move around some directories to be more consistent

--- src/main/java/dev/railroadide/railroad/project/data/MavenProjectKeys.java
+++
@@ -1,7 +0,0 @@
-package dev.railroadide.railroad.project.data;
-
-public class MavenProjectKeys {
-    public static final String GROUP_ID = "maven.groupId";
-    public static final String ARTIFACT_ID = "maven.artifactId";
-    public static final String VERSION = "maven.version";
-}
