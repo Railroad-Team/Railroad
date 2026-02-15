bcfb051caade9902f514ea665b98b2a1f2f24eb1
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2026-02-15 20:54:11 +0000

    Run commit list updates on JavaFX thread
    Schedule commitsList.setCommits(...) on the JavaFX Application Thread in both incoming and outgoing sync panes. When repoStatusProperty changes, the code now captures the commits into a List<GitCommit> and calls Platform.runLater to update the UI. Added imports for GitCommit, Platform and List and adjusted the listener bodies accordingly to ensure thread-safe UI updates.

--- src/main/java/dev/railroadide/railroad/ide/ui/git/sync/GitSyncIncomingChangesPane.java
+++ src/main/java/dev/railroadide/railroad/ide/ui/git/sync/GitSyncIncomingChangesPane.java
@@ -3,8 +3,12 @@ package dev.railroadide.railroad.ide.ui.git.sync;
 import dev.railroadide.core.ui.RRVBox;
 import dev.railroadide.core.ui.localized.LocalizedText;
 import dev.railroadide.railroad.vcs.git.GitManager;
+import dev.railroadide.railroad.vcs.git.commit.GitCommit;
+import javafx.application.Platform;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class GitSyncIncomingChangesPane extends RRVBox {
     public GitSyncIncomingChangesPane(GitManager gitManager) {
@@ -16,8 +20,10 @@ public class GitSyncIncomingChangesPane extends RRVBox {
         var commitsList = new GitSyncCommitsList();
         commitsList.setNoCommitsText("railroad.git.sync.incoming_changes.no_commits");
         commitsList.setCommits(gitManager.getIncomingCommits());
-        gitManager.repoStatusProperty().addListener((observable, oldValue, newValue) ->
-            commitsList.setCommits(gitManager.getIncomingCommits()));
+        gitManager.repoStatusProperty().addListener((observable, oldValue, newValue) -> {
+            List<GitCommit> incomingCommits = gitManager.getIncomingCommits();
+            Platform.runLater(() -> commitsList.setCommits(incomingCommits));
+        });
 
         getChildren().addAll(title, commitsList);
         VBox.setVgrow(commitsList, Priority.ALWAYS);
--- src/main/java/dev/railroadide/railroad/ide/ui/git/sync/GitSyncOutgoingChangesPane.java
+++ src/main/java/dev/railroadide/railroad/ide/ui/git/sync/GitSyncOutgoingChangesPane.java
@@ -3,8 +3,12 @@ package dev.railroadide.railroad.ide.ui.git.sync;
 import dev.railroadide.core.ui.RRVBox;
 import dev.railroadide.core.ui.localized.LocalizedText;
 import dev.railroadide.railroad.vcs.git.GitManager;
+import dev.railroadide.railroad.vcs.git.commit.GitCommit;
+import javafx.application.Platform;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class GitSyncOutgoingChangesPane extends RRVBox {
     public GitSyncOutgoingChangesPane(GitManager gitManager) {
@@ -16,8 +20,10 @@ public class GitSyncOutgoingChangesPane extends RRVBox {
         var commitsList = new GitSyncCommitsList();
         commitsList.setNoCommitsText("railroad.git.sync.outgoing_changes.no_commits");
         commitsList.setCommits(gitManager.getOutgoingCommits());
-        gitManager.repoStatusProperty().addListener((observable, oldValue, newValue) ->
-            commitsList.setCommits(gitManager.getOutgoingCommits()));
+        gitManager.repoStatusProperty().addListener((observable, oldValue, newValue) -> {
+            List<GitCommit> outgoingCommits = gitManager.getOutgoingCommits();
+            Platform.runLater(() -> commitsList.setCommits(outgoingCommits));
+        });
 
         getChildren().addAll(title, commitsList);
         VBox.setVgrow(commitsList, Priority.ALWAYS);
